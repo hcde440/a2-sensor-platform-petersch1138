83a62d28d6b52575baf2c00f86c5aaf3e4966d3c
Peter Schultz <aklitekpschultz@gmail.com>
2019-04-16 12:30:05 -0700

    Update A2.ino

--- A2.ino
+++ A2.ino
@@ -1,3 +1,5 @@
+#include <Adafruit_Si7021.h>
+
 
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
@@ -12,15 +14,27 @@ String windspeed;
 
 Servo servo;
 
-#define SERVO_PIN 2
+#define SERVO_PIN 2 // defines pin that servo uses
+typedef struct { // Weatherdata struct object contains string value pairs humidity, wind speed
+  String hd;
+  String ws;
 
-// set up the 'servo' feed
+} WData;
+
+WData weather;
+// set up the feeds
 AdafruitIO_Feed *servo_feed = io.feed("servo");
+AdafruitIO_Feed *local_humidity = io.feed("localHum");
+AdafruitIO_Feed *city_humidity = io.feed("humidity");
+AdafruitIO_Feed *wind_speed = io.feed("wind_speed");
 
+
+Adafruit_Si7021 sensor; // instantiate sensor var
 // Peter Schultz HCDE 440 4/16/19
 // This script takes in a city name from Adafruit IO that a user can enter on the dashboard, and
 // finds the wind speed in that city using the weather API that we used before, and rotates a 
-// micro servo with a speed that reflects the wind speed in that city.
+// micro servo with a speed that reflects the wind speed in that city. It also uses an I2C humidity
+// sensor, and compares the local humidity to the humidity taken from the weather API.
 void setup() {
   Serial.begin(115200);
   delay(100);
@@ -41,7 +55,8 @@ void setup() {
   Serial.println(); Serial.println("WiFi connected"); Serial.println();
   Serial.print("Your ESP has been assigned the internal IP address ");
   Serial.println(WiFi.localIP()); // local/internal ip address from wifi router
-  
+  sensor = Adafruit_Si7021(); // initialize i2c humidity sensor
+  sensor.begin(); // turn on sensor
   // connect to io.adafruit.com
   Serial.print("Connecting to Adafruit IO");
   io.connect();
@@ -59,8 +74,8 @@ void setup() {
   // we are connected
   Serial.println();
   Serial.println(io.statusText());
-  servo_feed->get();
-  servo.attach(SERVO_PIN);
+  servo_feed->get(); // gets data from the adafruit io website for the city name
+  servo.attach(SERVO_PIN); // tells servo to get data from this pin
 }
 
 void loop() {
@@ -80,18 +95,44 @@ void handleMessage(AdafruitIO_Data *data) {
 
   // convert the data to integer
   String city = data->toString(); // converts incoming data from Adafruit IO to a string
+  getWeather(city); // gets the wind speed in the given city
+
+  Serial.println("**********************************************");
+
   Serial.print("Windspeed in ");
   Serial.print(city);
   Serial.print(": ");
+  Serial.print(weather.ws);
+  Serial.println(" mph.");
+  Serial.println("");
 
-  windspeed = getSpeed(city); // gets the wind speed in the given city
-  Serial.println(windspeed);
+  Serial.print("Humidity in ");
+  Serial.print(city);
+  Serial.print(": ");
+  Serial.print(weather.hd);
+  Serial.println("%");
 
+  Serial.print("Local humidity: ");
+  int localHum = sensor.readHumidity(); // get humidity from si7021 sensor
+  Serial.print(localHum);
+  Serial.println("%");
+  Serial.println("");
+
+
+  Serial.print("Difference in humidities: ");
+  Serial.print(abs(localHum - weather.hd.toInt())); // find difference between local humidity and city humidity
+  Serial.println("%.");
+  Serial.println("");
+
+  local_humidity->save(localHum); // saves data to adafruit io
+  city_humidity->save(weather.hd);
+  wind_speed->save(weather.ws);
+
+  Serial.println("moving servo");
   int delayTime = (15-windspeed.toInt()); // sets the delay time, the faster the wind speed, the faster the rotation
   if(delayTime < 2) { // 15 is the default if there is no wind, and 2 is the minimum delay time (maximum rotation speed)
     delayTime = 2; 
   }
-
   int pos = 0;
   for (int i = 0; i < 5; i++) {
     for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
@@ -108,7 +149,7 @@ void handleMessage(AdafruitIO_Data *data) {
 
 
 
-String getSpeed(String city) { // this method gets the wind speed in the given city from the weather API 
+void getWeather(String city) { // this method gets the wind speed in the given city from the weather API 
   HTTPClient theClient; // initializes browser
   String apiCall = "http://api.openweathermap.org/data/2.5/weather?q=" + city; // these lines construct api address
   apiCall += "&units=imperial&appid=";
@@ -123,14 +164,13 @@ String getSpeed(String city) { // this method gets the wind speed in the given c
       JsonObject& root = jsonBuffer.parseObject(payload); // jsonObject contains the json data
       if (!root.success()) {
         Serial.println("parseObject() failed in getSpeed().");
-        return "";
+        return;
       }
       
-      return root["wind"]["speed"].as<String>(); // grabs speed of wind from the json format string 
+      weather.hd = root["main"]["humidity"].as<String>(); // grabs humidity from json format string
+      weather.ws = root["wind"]["speed"].as<String>(); // grabs speed of wind from the json format string 
     }
-  }
-  else {
+  } else {
     Serial.printf("Something went wrong with connecting to the endpoint in getSpeed().");
-    return "";
   }
 }
